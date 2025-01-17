107820448ba004b2debf6b76cf43a1cf1f7c7c06
bwilmet <benjamin.wilmet@minfin.fed.be>
2025-01-17 21:49:04 +0100

    In memory todos

--- database.py
+++ database.py
@@ -1,3 +1,19 @@
+import uuid
+
 from flask_jwt_extended import JWTManager
+
+USERS = {
+    "1": ['1',"admin","1234","admin"],
+    "2": ['2',"writer","1234","writer"],
+    "3": ['3',"reader","1234","reader"],
+    "4": ['4',"natatlia","1234","admin"],
+}
+
+TODOS = {
+    "1": [[str(uuid.uuid4()),"Test",0,1]],
+    "2": [],
+    "3": [],
+    "4": [],
+}
 
 jwt = JWTManager()
--- models/models.py
+++ models/models.py
@@ -1,5 +1,8 @@
 import uuid
 
+from database import USERS, TODOS
+
+
 class User:
 
     def __init__(self, the_id:int, username:str, password:str,role:str):
@@ -17,12 +20,7 @@ class User:
 
     @staticmethod
     def get_users():
-        with open("api/datas/users.txt", "r") as f:
-            content = f.read()
-            users = content.strip().split("\n")
-            for i in range(0,len(users)):
-                users[i] = users[i].split("|")
-        return users
+        return USERS
 
     @staticmethod
     def find_by_credentials(username, password):
@@ -39,8 +37,8 @@ class User:
         users = User.get_users()
         user = None
         for u in users:
-            if u[1] == username:
-                user = u
+            if users[u][1] == username:
+                user = users[u]
                 break
         return user
 
@@ -55,20 +53,10 @@ class Todo:
 
     @staticmethod
     def find_all_by_user_id(user_id: int):
-        try:
-            with open(f"api/datas//todos-{user_id}.txt", "r") as f:
-                content = f.read().strip()
-                if content != '':
-                    todos = content.strip().split("\n")
-                    for i in range(0, len(todos)):
-                        todos[i] = todos[i].split("|")
-                else:
-                    return None
-        except FileNotFoundError:
-            with open(f"api/datas/todos-{user_id}.txt", "w") as f:
-                f.write("")
-            todos = []
-        return todos
+        if TODOS[str(user_id)]:
+            return TODOS[str(user_id)]
+        else:
+            return None
 
     @staticmethod
     def find_by_id(todo_id:str, user_id:int):
@@ -76,53 +64,38 @@ class Todo:
         todo = None
         for i in range(0,len(todos)):
             if todos[i][0] == todo_id:
-                todo = Todo(todos[i][0], todos[i][1],todos[i][2] == '1', int(todos[i][3]))
-                break
+                return i
         return todo
 
     @staticmethod
     def update(t_id, user_id, datas):
-        todo_to_save = Todo.find_by_id(t_id, user_id)
-        if todo_to_save is not None:
-            todos = Todo.find_all_by_user_id(user_id)
-            todo_to_save.title = datas["title"]
-            todo_to_save.done = (0,1)[datas["done"]]
-            with open(f"api/datas/todos-{user_id}.txt", "w") as f:
-                content = ""
-                for todo in todos:
-                    if todo[0] == todo_to_save.id:
-                        content += f"{todo_to_save.id}|{todo_to_save.title}|{(0,1)[todo_to_save.done]}|{todo_to_save.user_id}\n"
-                    else:
-                        content += f"{todo[0]}|{todo[1]}|{todo[2]}|{todo[3]}\n"
-                f.write(content.strip())
-            return todo_to_save.to_dict()
+        todo_index = Todo.find_by_id(t_id, user_id)
+        if todo_index is not None:
+            TODOS[str(user_id)][todo_index][1] = datas['title']
+            TODOS[str(user_id)][todo_index][2] = (0,1)[datas["done"]]
+            todo =  Todo(
+                TODOS[str(user_id)][todo_index][0],
+                TODOS[str(user_id)][todo_index][1],
+                TODOS[str(user_id)][todo_index][2] == 1,
+                TODOS[str(user_id)][todo_index][3]
+            )
+            return todo.to_dict()
         else:
             return None
 
     @staticmethod
     def add(datas):
-        todos = Todo.find_all_by_user_id(datas['user_id'])
+        todos = TODOS[str(datas['user_id'])]
         if todos is None:
             todos = []
-        with open(f"api/datas/todos-{datas['user_id']}.txt", "w") as f:
-            new_todo = [str(uuid.uuid4()), datas['title'], ('0', '1')[datas['done']], str(datas['user_id'])]
-            todo_obj = Todo(new_todo[0], new_todo[1], new_todo[2] == '1', int(new_todo[3]))
-            todos.append(new_todo)
-            content = ""
-            for todo in todos:
-                content += f"{todo[0]}|{todo[1]}|{todo[2]}|{todo[3]}\n"
-            f.write(content.strip())
+        new_todo = [str(uuid.uuid4()), datas['title'], (0, 1)[datas['done']], datas['user_id']]
+        todo_obj = Todo(new_todo[0], new_todo[1], new_todo[2] == 1, int(new_todo[3]))
+        TODOS[str(datas['user_id'])].append(new_todo)
         return todo_obj.to_dict()
 
     @staticmethod
     def delete(t_id, user_id):
-        todos = Todo.find_all_by_user_id(user_id)
-        with open(f"api/datas/todos-{user_id}.txt", "w") as f:
-            content = ""
-            for t in todos:
-                if t[0] != t_id:
-                    content += f"{t[0]}|{t[1]}|{t[2]}|{t[3]}\n"
-            f.write(content)
+        del TODOS[str(user_id)][Todo.find_by_id(t_id, user_id)]
         return True
 
     def to_dict(self):
@@ -132,3 +105,4 @@ class Todo:
             "done": self.done,
             "user_id": self.user_id,
         }
+

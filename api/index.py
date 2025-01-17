6b287725f66b0b026327d2310c5e88103e35eddf
bwilmet <benjamin.wilmet@minfin.fed.be>
2025-01-17 19:32:55 +0100

    Fixed backend ?

--- api/index.py
+++ api/index.py
@@ -5,17 +5,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, decode_token
 
-from database import db, jwt
+from database import jwt
 from datas.i18n import get_i18n
 from models.models import User, Todo
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = "zaaa25Tefek58ynbqm658dziPsn"
 app.config['JWT_VERIFY_SUB'] = False
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=3)
-db.init_app(app)
 jwt.init_app(app)
 CORS(app)
 
@@ -27,8 +24,6 @@ def token_required(f):
             token = request.headers['Authorization'].split(" ")[1] # Assuming format `Bearer <token>`
         if not token:
             return jsonify({'message': 'Token is missing!'}), 403
-        data = get_jwt_identity()
-        print(data)
         return f(*args, **kwargs)
     return decorated
 
@@ -38,10 +33,11 @@ def login(lang: str):
     datas = request.get_json()
     if not datas.get('username') or not datas.get("password"):
         return jsonify({"msg": get_i18n(lang, "MISSING_CREDENTIAL")}), 400
-    user = User.find_by_credentials(username=datas.get('username'), password=datas.get("password")).first()
+    user = User.find_by_credentials(username=datas.get('username'), password=datas.get("password"))
     if not user:
         return jsonify({"msg": get_i18n(lang, "WRONG_CREDENTIALS")}), 404
     else:
+        user = User(user[0], user[1], user[2], user[3])
         return jsonify({
             "user": user.to_dict(),
             "access_token": create_access_token(identity={'username': user.username, 'role': user.role})}), 200
@@ -51,33 +47,37 @@ def login(lang: str):
 @jwt_required()
 def get_todos(lang: str):
     token = request.headers["Authorization"].replace("Bearer ", "")
-    user = User.find_by_username(decode_token(token)["sub"]["username"]).first()
-    res = Todo.find_all_by_user_id(user.id)
+    user = User.find_by_username(decode_token(token)["sub"]["username"])
+    res = Todo.find_all_by_user_id(user[0])
+    if res is None:
+        return jsonify({"todos": []})
     todos = []
     for todo in res:
-        todos.append(todo.to_dict())
+        todo_obj = Todo(todo[0], todo[1], todo[2] == '1', int(todo[3]))
+        todos.append(todo_obj.to_dict())
     return jsonify({"todos": todos})
 
 @app.route("/api/<lang>/todos", methods=["POST"])
 @jwt_required()
 def post_todos(lang: str):
-    user = User.find_by_username(get_jwt_identity()["username"]).first()
+    user = User.find_by_username(get_jwt_identity()["username"])
     json = request.get_json()
-    json['user_id'] = user.id
+    json['user_id'] = user[0]
     todo = Todo.add(json)
     return jsonify({"todo": todo})
 
-@app.route("/api/<lang>/todos/<int:todo_id>", methods=["PUT"])
+@app.route("/api/<lang>/todos/<todo_id>", methods=["PUT"])
 @jwt_required()
 def put_todos(lang: str, todo_id: int):
+    token = request.headers["Authorization"].replace("Bearer ", "")
+    user = User.find_by_username(decode_token(token)["sub"]["username"])
+    return jsonify({"todo": Todo.update(todo_id, user[0], request.get_json())})
 
-    return jsonify({"todo": Todo.update(todo_id, request.get_json())})
-
-@app.route("/api/<lang>/todos/<int:todo_id>", methods=["DELETE"])
+@app.route("/api/<lang>/todos/<todo_id>", methods=["DELETE"])
 @jwt_required()
 def delete_todos(lang: str, todo_id: int):
-    user = User.find_by_username(get_jwt_identity()["username"]).first()
-    return jsonify({"todo": Todo.delete(todo_id, user.id)})
+    user = User.find_by_username(get_jwt_identity()["username"])
+    return jsonify({"todo": Todo.delete(todo_id, user[0])})
 
 @app.route("/api/<lang>/contact", methods=["POST"])
 @jwt_required()
@@ -87,6 +87,4 @@ def submit_contact_form(lang: str):
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
--- models/models.py
+++ models/models.py
@@ -1,13 +1,12 @@
-from database import db
+import uuid
 
-class User(db.Model):
-    __tablename__ = 'users'
+class User:
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(10))  # 'writer' or 'viewer'
-    todos = db.relationship('Todo', backref='user', lazy=True)
+    def __init__(self, the_id:int, username:str, password:str,role:str):
+        self.id = the_id
+        self.username = username
+        self.password = password
+        self.role = role
 
     def to_dict(self):
         return {
@@ -17,54 +16,114 @@ class User(db.Model):
         }
 
     @staticmethod
+    def get_users():
+        with open("../datas/users.txt", "r") as f:
+            content = f.read()
+            users = content.strip().split("\n")
+            for i in range(0,len(users)):
+                users[i] = users[i].split("|")
+        return users
+
+    @staticmethod
     def find_by_credentials(username, password):
-        return User.query.filter_by(username=username, password=password)
+        users = User.get_users()
+        user = None
+        for u in users:
+            if u[1] == username and u[2] == password:
+                user = u
+                break
+        return user
 
     @staticmethod
     def find_by_username(username):
-        return User.query.filter_by(username=username)
+        users = User.get_users()
+        user = None
+        for u in users:
+            if u[1] == username:
+                user = u
+                break
+        return user
 
 
-class Todo(db.Model):
-    __tablename__ = 'todos'
+class Todo:
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    done = db.Column(db.Boolean, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    def __init__(self, the_id:str, title:str, done:bool,user_id:int):
+        self.id = the_id
+        self.title = title
+        self.done = done
+        self.user_id = user_id
 
     @staticmethod
     def find_all_by_user_id(user_id: int):
-        return Todo.query.filter_by(user_id=user_id)
+        try:
+            with open(f"../datas/todos-{user_id}.txt", "r") as f:
+                content = f.read().strip()
+                if content != '':
+                    todos = content.strip().split("\n")
+                    for i in range(0, len(todos)):
+                        todos[i] = todos[i].split("|")
+                else:
+                    return None
+        except FileNotFoundError:
+            with open(f"../datas/todos-{user_id}.txt", "w") as f:
+                f.write("")
+            todos = []
+        return todos
 
     @staticmethod
-    def find_by_id(todo_id: int):
-        return Todo.query.filter_by(id=todo_id)
+    def find_by_id(todo_id:str, user_id:int):
+        todos = Todo.find_all_by_user_id(user_id)
+        todo = None
+        for i in range(0,len(todos)):
+            if todos[i][0] == todo_id:
+                todo = Todo(todos[i][0], todos[i][1],todos[i][2] == '1', int(todos[i][3]))
+                break
+        return todo
 
     @staticmethod
-    def update(t_id, datas):
-        todo = Todo.find_by_id(t_id).first()
-        todo.title = datas["title"]
-        todo.done = datas["done"]
-        db.session.commit()
-        return todo.to_dict()
+    def update(t_id, user_id, datas):
+        todo_to_save = Todo.find_by_id(t_id, user_id)
+        if todo_to_save is not None:
+            todos = Todo.find_all_by_user_id(user_id)
+            todo_to_save.title = datas["title"]
+            todo_to_save.done = (0,1)[datas["done"]]
+            with open(f"../datas/todos-{user_id}.txt", "w") as f:
+                content = ""
+                for todo in todos:
+                    if todo[0] == todo_to_save.id:
+                        content += f"{todo_to_save.id}|{todo_to_save.title}|{(0,1)[todo_to_save.done]}|{todo_to_save.user_id}\n"
+                    else:
+                        content += f"{todo[0]}|{todo[1]}|{todo[2]}|{todo[3]}\n"
+                f.write(content.strip())
+            return todo_to_save.to_dict()
+        else:
+            return None
 
     @staticmethod
     def add(datas):
-        todo = Todo()
-        todo.title = datas['title']
-        todo.done = datas['done']
-        todo.user_id = datas['user_id']
-        db.session.add(todo)
-        db.session.commit()
-        return todo.to_dict()
+        todos = Todo.find_all_by_user_id(datas['user_id'])
+        if todos is None:
+            todos = []
+        with open(f"../datas/todos-{datas['user_id']}.txt", "w") as f:
+            new_todo = [str(uuid.uuid4()), datas['title'], ('0', '1')[datas['done']], str(datas['user_id'])]
+            todo_obj = Todo(new_todo[0], new_todo[1], new_todo[2] == '1', int(new_todo[3]))
+            todos.append(new_todo)
+            content = ""
+            for todo in todos:
+                content += f"{todo[0]}|{todo[1]}|{todo[2]}|{todo[3]}\n"
+            f.write(content.strip())
+        return todo_obj.to_dict()
 
     @staticmethod
     def delete(t_id, user_id):
-        todo = Todo.find_by_id(t_id).filter_by(user_id=user_id).first()
-        db.session.delete(todo)
-        db.session.commit()
-        return todo.to_dict()
+        todos = Todo.find_all_by_user_id(user_id)
+        with open(f"../datas/todos-{user_id}.txt", "w") as f:
+            content = ""
+            for t in todos:
+                if t[0] != t_id:
+                    content += f"{t[0]}|{t[1]}|{t[2]}|{t[3]}\n"
+            f.write(content)
+        return True
 
     def to_dict(self):
         return {
